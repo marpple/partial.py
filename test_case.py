3cbbcfac3b7b51b99c82cf3c8c4fd2f68aa96660
Joeun <imjoeunha@gmail.com>
2017-05-10 15:50:16 +0900

    update
    - test_case

--- test_case.py
+++ test_case.py
@@ -2,101 +2,99 @@
 from partial import _, __, ___
 
 
-test_case = [
-    {
-        "title": "flatten1",
+test_case = {
+    "flatten1": {
         "method": _.flatten,
         "args": [ [1, [2], [3, [[4]]]] ],
         "expect": [1, 2, 3, 4]
     },
-    {
-        "title": "flatten2",
+    "flatten2": {
         "method": _.flatten,
         "args": [ [1, [2], [3, [[4]]]], True ],
         "expect": [1, 2, 3, [[4]]]
     },
-    {
-        "title": "difference",
+    "difference": {
         "method": _.difference,
         "args": [ [1, 2, 3, 4, 5], [5, 2, 10] ],
         "expect": [1, 3, 4]
     },
-    {
-        "title": "without",
+    "without": {
         "method": _.without,
         "args": [ [1, 2, 1, 0, 3, 1, 4], 0, 1 ],
         "expect": [2, 3, 4]
     },
-    {
-        "title": "union",
+    "union": {
         "method": _.union,
         "args": [ [1, 2, 3], [101, 2, 1, 10], [2, 1] ],
         "expect": [1, 2, 3, 101, 10]
     },
-    {
-        "title": "intersection",
+    "intersection": {
         "method": _.intersection,
         "args": [ [1, 2, 3], [101, 2, 1, 10], [2, 1] ],
         "expect": [1, 2]
     },
-    {
-        "title": "uniq1",
+    "uniq1": {
         "method": _.uniq,
         "args": [ [1, 2, 1, 4, 1, 3] ],
         "expect": [1, 2, 4, 3]
     },
-    {
-        "title": "uniq2",
+    "uniq2": {
         "method": _.uniq,
         "args": [ [1, 2, 1, 4, 1, 3], lambda v, *rest: v % 2 ],
         "expect": [1, 2]
     },
-    {
-        "title": "zip1",
+    "zip1": {
         "method": _.zip,
         "args": [ ['moe', 'larry', 'curly'], [30, 40, 50], [True, False, False] ],
         "expect": [["moe", 30, True], ["larry", 40, False], ["curly", 50, False]]
     },
-    # {
-    #     "title": "zip2",
+    # "zip2": {
     #     "method": _.zip,
     #     "args": [ ['moe', 'larry', 'curly'], [30, 40, 50], [True, False, False], ['KKK'] ],
     #     "expect": [["moe", 30, True, "KKK"], ["larry", 40, False, None], ["curly", 50, False, None]]
     # },
-    {
-        "title": "unzip1",
+    "unzip1": {
         "method": _.unzip,
         "args": [ [["moe", 30, True], ["larry", 40, False], ["curly", 50, False]] ],
         "expect": [['moe', 'larry', 'curly'], [30, 40, 50], [True, False, False]]
     },
-    {
-        "title": "unzip2",
-        "method": _.unzip,
-        "args": [ [["moe", 30, True], ["larry", 40, False], ["curly", 50, False, "KKK"]] ],
-        "expect": [['moe', 'larry', 'curly'], [30, 40, 50], [True, False, False], [None, None, 'KKK']]
-    },
-    {
-        "title": "max1",
+    # "unzip2": {
+    #     "method": _.unzip,
+    #     "args": [ [["moe", 30, True], ["larry", 40, False], ["curly", 50, False, "KKK"]] ],
+    #     "expect": [['moe', 'larry', 'curly'], [30, 40, 50], [True, False, False], [None, None, 'KKK']]
+    # },
+    "max1": {
         "method": _.max,
         "args": [ [1,2,3,4,5] ],
         "expect": 5
     },
-    {
-        "title": "max2",
+    "max2": {
         "method": _.max,
         "args": [ [{'name': 'moe', 'age': 40}, {'name': 'larry', 'age': 50}, {'name': 'curly', 'age': 60}], lambda v, *r: v['age'] ],
         "expect": {'name': 'curly', 'age': 60}
     }
-]
+}
 
 
-def test(cases):
-    for case in cases:
-        if case["method"](*case["args"]) == case["expect"]:
-            print("%-13s: PASS" % case["title"])
+def test(cases, key=None):
+    def _t(method, args, expect, title):
+        if method(*args) == expect:
+            print("%-13s: PASS" % title)
         else:
-            print("%-13s: FAIL" % case["title"])
-            print("## expect:", case["expect"], "/ result:", case["method"](*case["args"]), "##")
+            print("%-13s: FAIL" % title)
+            print("## expect:", expect, "/ result:", method(*args), "##")
 
+    keys = cases.keys()
+    if key and key in keys:
+        _t(cases[key]['method'], cases[key]['args'], cases[key]['expect'], key)
+        return
 
+    if key and key not in keys:
+        print("not found '%s'" % key)
+        return
+
+    for key in keys:
+        _t(cases[key]['method'], cases[key]['args'], cases[key]['expect'], key)
+
+# test(test_case, 'zip1')
 test(test_case)
